f19d6ac6c4478b9d6d4a51220fc4e4a311825ea0
VladRM <vladxrosca@gmail.com>
2025-05-11 01:45:33 +0300

    upd

--- gameConfig.js
+++ gameConfig.js
@@ -4,5 +4,5 @@ export const DISPLAYED_GROUND_HEIGHT = 20;
 export const GAME_SPEED = 250; // Base speed of the game
 export const OBSTACLE_SPAWN_DELAY = 1750; // Delay for combined spawner
 export const MAX_LEVELS = 5; // Number of levels before the voting booth appears
-export const LEVEL_DURATION_MS = 20000; // Duration of each level in milliseconds
+export const LEVEL_DURATION_MS = 5000; // Duration of each level in milliseconds
 export const ENEMY_TO_TRAP_RATIO = 2; // Enemies spawn twice as often as traps (e.g., 2 means 2 enemies per 1 trap)
