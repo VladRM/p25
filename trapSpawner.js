5b7d79a9a446c75293e58145e4daf73c8d957796
VladRM <vladxrosca@gmail.com>
2025-05-13 09:58:42 +0300

    traps

--- trapSpawner.js
+++ trapSpawner.js
@@ -10,10 +10,10 @@ export class TrapSpawner {
 
     spawnTrap() {
         const TRAP_TYPES = [
-            { name: 'manipulated', type: 'sprite', textureKey: 'trap_2_a', message_disarmed: "Ai folosit gândirea critică!", message_passed_by: "Nu trece nepăsător pe lângă cei nehotărâți!" }, // Sprite-based
-            { name: 'direction', type: 'sprite', textureKey: 'trap_1_a', message_disarmed: "Bravo, le-ai arătat direcția!", message_passed_by: "Ajută-i pe cei dezorientați să găsească calea!" }, // Sprite-based
-            { name: 'blind', type: 'sprite', textureKey: 'trap_3_a', message_disarmed: "Felicitări! Ai făcut lumină și s-au decis!", message_passed_by: "Nu-i lăsa pe apropiați în întuneric!" }, // Sprite-based
-            { name: 'groupthink', type: 'sprite', textureKey: 'trap_4_a', message_disarmed: "Felicitări! Ai făcut lumină și s-au decis!", message_passed_by: "Nu-i lăsa pe apropiați în întuneric!" } // Sprite-based, uses darkweb messages
+            { name: 'direction', type: 'sprite', textureKey: 'trap_1_a', message_disarmed: "Bravo, le-ai arătat direcția!", message_passed_by: "Ajută-i pe cei dezorientați să găsească calea!" },
+            { name: 'manipulated', type: 'sprite', textureKey: 'trap_2_a', message_disarmed: "Ai folosit gândirea critică!", message_passed_by: "Nu trece nepăsător pe lângă cei nehotărâți!" },
+            { name: 'blind', type: 'sprite', textureKey: 'trap_3_a', message_disarmed: "Ti-ai folosit viziunea și ai luminat drumul!", message_passed_by: "Fără tine, rămân orbiți de propagandă. Folosește-ți viziunea!" },
+            { name: 'groupthink', type: 'sprite', textureKey: 'trap_4_a', message_disarmed: "Felicitări! Ai făcut lumină și s-au decis!", message_passed_by: "Nu-i lăsa pe apropiați în întuneric!" }
         ];
         let selectedTrapType;
         do {
